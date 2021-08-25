0389c7f0500abd6fe30552767cb6771278f4a402
Viera Garcia <vieravfg@gmail.com>
2021-08-24 22:30:32 -0400

    pages and vendor js

--- atlanta_crime/app.py
+++ atlanta_crime/app.py
@@ -92,6 +92,29 @@ def customers():
         })
         return jsonify(data={"status":201,"msg":"You added a customer"})
 
+# create routes that render crime pages
+#################################################
+
+@app.route("/page1")
+def page1():
+    return render_template("page1.html")
+
+@app.route("/page2")
+def page2():
+    return render_template("page2.html")
+
+@app.route("/page3")
+def page3():
+    return render_template("page3.html")
+
+@app.route("/page4")
+def page4():
+    return render_template("page4.html")
+    
+@app.route("/page5")
+def page5():
+    return render_template("page5.html")
+#################################################
 
 if __name__ == "__main__":
     app.run(debug = True)
